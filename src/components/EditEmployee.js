acb187d441cc2085a00f996517e8126eba937d13
Elizabeth <elizabethadebayo269@gmail.com>
2023-12-15 10:19:15 +0100

    added new modal

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -1,41 +1,84 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-function Employee() {
-    const [show, setShow] = useState(false);
+function EditEmployee() {
+	const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
-    return (
-    <>
-        <Button variant="primary" onClick={handleShow}>
-            Launch static backdrop modal
-        </Button>
+	return (
+		<>
+			<button
+				onClick={handleShow}
+				class='px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white 
+                    hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'>
+				Update
+			</button>
 
-        <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-        >
-        <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            I will not close if you click outside me. Do not even try to press
-            escape key.
-        </Modal.Body>
-        <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-            Close
-            </Button>
-            <Button variant="primary">Understood</Button>
-        </Modal.Footer>
-        </Modal>
-    </>
-    );
+			<Modal
+				show={show}
+				onHide={handleClose}
+				backdrop='static'
+				keyboard={false}>
+				<Modal.Header closeButton>
+					<Modal.Title>Update Employee</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<form class='w-full max-w-sm'>
+						<div class='md:flex md:items-center mb-6'>
+							<div class='md:w-1/3'>
+								<label
+									class='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+									for='inline-full-name'>
+									Full Name
+								</label>
+							</div>
+							<div class='md:w-2/3'>
+								<input
+									class='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+									id='inline-full-name'
+									type='text'
+									value='Jane Doe'
+								/>
+							</div>
+						</div>
+						<div class='md:flex md:items-center mb-6'>
+							<div class='md:w-1/3'>
+								<label
+									class='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+									for='inline-password'>
+									Role
+								</label>
+							</div>
+							<div class='md:w-2/3'>
+								<input
+									class='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+									id='inline-password'
+									type='text'
+									placeholder='Jane doe'
+								/>
+							</div>
+						</div>
+					</form>
+				</Modal.Body>
+				<Modal.Footer>
+					<button
+						onClick={handleClose}
+						class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+						Close
+					</button>
+					<button
+						class='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
+						type='button'
+						form='Editmodal'>
+						Update
+					</button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
 }
 
-export default Employee;
+export default EditEmployee;
