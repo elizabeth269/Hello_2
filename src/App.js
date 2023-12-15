796704b3750fdd991501e978d389272da80095d4
Elizabeth <elizabethadebayo269@gmail.com>
2023-12-15 04:49:25 +0100

    added new files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 
 function App() {
@@ -9,12 +9,17 @@ function App() {
     <div className="App">
       <input type='text' onChange={(e) => {console.log(e.target.value)
       setRole(e.target.value)}}/>
-      {showEmployees ? (<><Employee/>
-        <Employee name='caleb' role='dev'/>
-        <Employee  name='card' role='green'/>
-        <Employee name='abby' role={role}/>
-        <Employee/>
-        <Employee/></>): <>you cannot see the employees</>}
+      {showEmployees ? (<div className='flex flex-wrap justify-center'>
+        <Employee name='caleb' role='dev' img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee  name='card' role='green' img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee name='abby' role={role} img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee  name='card' role='green' img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee name='abby' role={role} img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee  name='card' role='green' img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee name='abby' role={role} img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee name='abby' role={role} img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee name='abby' role={role} img= 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=300'/>
+        <Employee/></div>): <>you cannot see the employees</>}
         
     </div>
   );
